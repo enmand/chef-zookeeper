f88c676dff7cdabbbdce32c5efda2f6a26859977
Edward Paget <ed.paget@gmail.com>
2014-02-06 11:57:46 -0600

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "zookeeper"
-maintainer       "Simple Finance Technology Corp."
-maintainer_email "mwhooker@simple.com"
+maintainer       "Edward Paget"
+maintainer_email "ed@zooniverse.org"
 license          "All rights reserved"
 description      "Installs/Configures zookeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.4.10"
+version          "1.4.11"
 
 depends          "java"
 depends          "build-essential"
