31a2e08a23fa7f6be2e41c36a16dd67cd99a9bac
Edward Paget <ed.paget@gmail.com>
2014-02-07 14:13:01 -0600

    Fix typo in zookeeper attributes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ed@zooniverse.org"
 license          "All rights reserved"
 description      "Installs/Configures zookeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.2"
+version          "2.0.3"
 
 depends          "java"
 depends          "build-essential"
