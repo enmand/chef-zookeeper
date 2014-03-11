b7dd4fb5b293492dfb30c2c2107a71244ff1bb2e
Edward Paget <ed.paget@gmail.com>
2014-03-11 13:29:36 -0500

    Remove unused files

--- attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:gradle][:version] = "1.5"
-default[:gradle][:mirror] = "http://services.gradle.org/distributions/gradle-#{default[:gradle][:version]}-bin.zip"
-default[:gradle][:checksum] = 'a5511a0659caa47d9d74fd2844c9da43157d2f78e63a0223c6289d88f5aaecbe'
