67f50d881fad63100e9dabc9aa693126ae184c11
Daniel Enman <enmand@unerror.com>
2014-03-24 23:57:42 -0300

    Remove java dep version

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      "Installs/Configures zookeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.1.1"
 
-depends          'java', '1.20.0'
+depends          'java'
 
 supports 'ubuntu'
